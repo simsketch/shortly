fba2f0980595e674176cba2033414e87cc5825c2
Amit Burstein <amit.burstein@gmail.com>
2015-09-15 22:47:03 -0700

    Add skeleton UI (without styling) and app logic

--- client/app.js
+++ client/app.js
@@ -5,7 +5,18 @@
 
   let app = angular.module('app', []);
 
-  app.controller('AppCtrl', ['$scope', function($scope) {
-    
+  app.controller('AppCtrl', ['$scope', '$http', '$window', function($scope, $http, $window) {
+    $scope.submit = function() {
+      // TODO(amitburst): Add URL, and other, validation.
+      if ($scope.longUrl) {
+        $http.post('/url', {longUrl: $scope.longUrl})
+          .then(function(response) {
+            $scope.shortUrl = $window.location.origin + '/' + response.data.shortUrl;
+          }, function(error) {
+            // TODO(amitburst): Handle errors.
+            console.log(error);
+          });
+      }
+    };
   }]);
 })();
