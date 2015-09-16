2f940014f3c8c8d0ed4a204a036b087a66b37bfa
Amit Burstein <amit.burstein@gmail.com>
2015-09-15 21:10:51 -0700

    Add frontend build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,48 @@
 
 let gulp = require('gulp');
 let jshint = require('gulp-jshint');
+let del = require('del');
+let sass = require('gulp-sass');
 let nodemon = require('gulp-nodemon');
+let useref = require('gulp-useref');
+let gulpif = require('gulp-if');
+let uglify = require('gulp-uglify');
+let minifyCss = require('gulp-minify-css');
 
 // Lints JavaScript files.
 gulp.task('lint', function() {
-  return gulp.src(['gulpfile.js', 'server/**/*.js'])
+  return gulp.src(['gulpfile.js', 'server/**/*.js', 'client/app.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// Cleans out the public directory.
+gulp.task('clean', function() {
+  del(['public']);
+});
+
+// Builds all necessary files.
+gulp.task('build', ['clean', 'lint'], function() {
+  let assets = useref.assets();
+  return gulp.src('client/index.html')
+    .pipe(assets)
+    .pipe(gulpif('*.css', sass()))
+    .pipe(gulpif('*.css', minifyCss()))
+    .pipe(gulpif('*.js', uglify()))
+    .pipe(assets.restore())
+    .pipe(useref())
+    .pipe(gulp.dest('public'));
 });
 
 // Uses Nodemon to restart the server when files change.
 gulp.task('develop', function() {
   nodemon({
     script: 'server/app.js',
-    ext: 'js',
-    ignore: ['node_modules/**/*.js'],
-    tasks: ['lint']
+    ext: 'js scss',
+    ignore: ['node_modules/**/*', 'client/bower_components/**/*', 'public/**/*'],
+    tasks: ['build']
   });
 });
+
+// Default gulp task.
+gulp.task('default', ['build']);
